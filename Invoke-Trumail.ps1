31f755bc460db18d9230473c4063166d28eacb0e
Rasta Mouse <rasta@rastamouse.me>
2018-07-09 16:14:23 +0100

    Invoke-Trumail

---
+++ Invoke-Trumail.ps1
@@ -0,0 +1,55 @@
+﻿<#
+
+Author: RastaMouse
+Version: 0.1
+
+Purpose: Trumail is an email address verification API.
+         This script takes an email list input and checks
+         them against the Trumail service.
+
+#>
+
+function Invoke-Trumail {
+
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$True)]
+    [string]$EmailFile
+  )
+
+  BEGIN {
+  
+    $EmailList = Get-Content -LiteralPath $EmailFile -Encoding Ascii
+    $EmailCount = ($EmailList | Measure-Object -Line).Lines
+    Write-Verbose "$($EmailCount) email(s) found in $($EmailFile)"
+
+    $Results = @()
+  
+  }
+
+  PROCESS {
+
+    $i = 0
+  
+    ForEach ($Email in $EmailList) {
+
+      $API = "534e2038-35dd-4286-82bf-e8c199a90669"
+    
+      $Req = Invoke-RestMethod -Method Get -Uri "https://api.trumail.io/v2/lookups/json?email=$($Email)&token=$($API)"
+      $Results += $Req
+
+      $i++
+      $Progress = [math]::Round($i/$EmailCount*100)
+      Write-Progress -Activity "Validating..." -PercentComplete $Progress -CurrentOperation "$($Progress)% Complete"
+    
+    }
+  
+  }
+
+  END {
+  
+    $Results | Out-GridView -Title "Trumail" -PassThru
+  
+  }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Invoke-Trumail
+
+## Intro
+
+> Trumail is a robust and powerful email address verification API written on top of some of the must cutting edge technologies. It was built with the intention of providing developers and businesses with a simple and easy to use solution to bounced emails.
+
+```json
+{
+    "address": "user@domain.com",
+    "username": "user",
+    "domain": "domain.com",
+    "validFormat": true,
+    "deliverable": true,
+    "fullInbox": false,
+    "hostExists": true,
+    "catchAll": true,
+    "disposable": false,
+    "free": false
+}
+```
+
+## Usage
+
+```
+PS C:\> gc .\emails.txt
+rasta@rastamouse.me
+doesnotexist@rastamouse.me
+
+PS C:\> ipmo .\Invoke-Trumail.ps1
+PS C:\> Invoke-Trumail -EmailFile .\emails.txt
+```
+
+![](GridView.png)
